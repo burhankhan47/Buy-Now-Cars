3aa0ba14984a9a3d1eae237f1ee48c7ac23ee2c4
burhankhan47 <122910584+burhankhan47@users.noreply.github.com>
2023-01-25 18:49:47 +0500

    js push

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-// alert ("Sell Your Car on auto mobile and Get the Best PriceSell Your Car on auto mobile and Get the Best Price")
+alert ("Sell Your Car on auto mobile and Get the Best PriceSell Your Car on auto mobile and Get the Best Price")
