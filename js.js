c039b574238db268219c5207b2aee94173dfb6d7
burhankhan47 <122910584+burhankhan47@users.noreply.github.com>
2023-02-14 19:06:16 +0500

    JS (even) or (odd)

--- js.js
+++ js.js
@@ -37,3 +37,30 @@ function Grades(){
 
 
 Grades()
+
+var passStudent = []
+var failStudent = []
+
+function CheckPassFail(stud) {
+
+    if (stud >= 50) {
+        passStudent.push(stud)
+    } else failStudent.push(stud)
+
+}
+
+var Student1 = +prompt('write your percentage')
+CheckPassFail(Student1)
+
+var Student2 = +prompt('write your percentage')
+CheckPassFail(Student2)
+
+var Student3 = +prompt('write your percentage')
+CheckPassFail(Student3)
+
+var Student4 = +prompt('write your percentage')
+CheckPassFail(Student4)
+
+console.log(passStudent)
+console.log(failStudent)
+
