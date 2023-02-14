4a282cc6b5746a4cd17a0c5428b40da9cd9db312
burhankhan47 <122910584+burhankhan47@users.noreply.github.com>
2023-02-14 21:08:43 +0500

    js table and loop

---
+++ Loop.js
@@ -0,0 +1,24 @@
+// LOOP 
+
+// Loops are handy, if you want to run the same code over and over again, each time with a different value
+
+        
+// for - loops
+var cities = ["karachi" , "lahore", "peshawar", "islamabad"]
+for(var i =1 ; i <= 10; i++){
+
+console.log('10 * ' + i + ' = ' + i * 10)
+    // console.log(cities[i])
+}
+
+// for (var x = 0; x <= 100; x++){
+//     console.log(cities[x])
+// }
+
+
+
+
+var TableNumber = prompt('type table number')
+ for( var x = 1;x <= 10; x++){
+    console.log(TableNumber + ' * ' + x + ' = ' +x * TableNumber)
+ }
--- index.js
+++ index.js
@@ -67,29 +67,3 @@ console.log(PartnersOfCar , "Number Of Car Partners ")
 var TotalCarPartners = AfterDiscountCarPrice / PartnersOfCar
 console.log(TotalCarPartners ,"Total Bill For Each Person")
 
-var passStudent = []
-var failStudent = []
-
-function CheckPassFail(stud) {
-
-    if (stud >= 50) {
-        passStudent.push(stud)
-    } else failStudent.push(stud)
-
-}
-
-var Student1 = +prompt('write your percentage')
-CheckPassFail(Student1)
-
-var Student2 = +prompt('write your percentage')
-CheckPassFail(Student2)
-
-var Student3 = +prompt('write your percentage')
-CheckPassFail(Student3)
-
-var Student4 = +prompt('write your percentage')
-CheckPassFail(Student4)
-
-console.log(passStudent)
-console.log(failStudent)
-
